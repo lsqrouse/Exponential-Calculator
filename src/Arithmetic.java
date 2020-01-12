acfd78c1fb0b9e6edee0c331c21a7b22e211d9d9
lsqrouse <rouse8@purdue.edu>
2020-01-12 17:02:32 -0500

    base implementation of handing the four main operations, with dividebyzero exception

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -12,16 +12,58 @@ public class Arithmetic {
         return n1 * n2;
     }
 
-    public static double divide(double n1, double n2) {
+    public static double divide(double n1, double n2) throws DivideByZeroException{
+        if(n2 == 0)
+            throw new DivideByZeroException("Divide by Zero");
         return n1 / n2;
     }
 
-    public static double solve(String todo) {
-        String noSpace = noSpace(todo);
-        for (int i = 0; i < todo.length(); i++) {
-            char c = todo.charAt(i);
-            if(c == '(' || c == '+' || c == '-' || c == '/') {
-                stack.push(Character.toString(todo.charAt(i++)));
+    public static double solve(String s) throws DivideByZeroException{
+        String toParse = noSpace(s);
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < toParse.length(); i++) {
+            char c = toParse.charAt(i);
+            double n2 = Double.parseDouble(Character.toString(toParse.charAt(i + 1)));
+            if(Character.isDigit(c) && i == 0)
+                stack.push(Character.toString(c));
+            if(c == '(')
+                stack.push(Character.toString(toParse.charAt(1 + i)));
+            else if (c == '+') {
+                try {
+                    double n1 = Double.parseDouble(stack.pop());
+                    stack.push(Double.toString(add(n1, n2)));
+                } catch (NumberFormatException e) {
+                    double n1 = Double.parseDouble(stack.pop());
+                    n2 = solve(toParse.substring(i));
+                    stack.push(Double.toString(add(n1, n2)));
+                }
+            } else if (c == '-') {
+                try {
+                    double n1 = Double.parseDouble(stack.pop());
+                    stack.push(Double.toString(subtract(n1, n2)));
+                } catch (NumberFormatException e) {
+                    double n1 = Double.parseDouble(stack.pop());
+                    n2 = solve(toParse.substring(i));
+                    stack.push(Double.toString(subtract(n1, n2)));
+                }
+            } else if (c == '/') {
+                try {
+                    double n1 = Double.parseDouble(stack.pop());
+                    stack.push(Double.toString(subtract(n1, n2)));
+                } catch (NumberFormatException e) {
+                    double n1 = Double.parseDouble(stack.pop());
+                    n2 = solve(toParse.substring(i));
+                    stack.push(Double.toString(divide(n1, n2)));
+                }
+            } else if (c == '*') {
+                try {
+                    double n1 = Double.parseDouble(stack.pop());
+                    stack.push(Double.toString(subtract(n1, n2)));
+                } catch (NumberFormatException e) {
+                    double n1 = Double.parseDouble(stack.pop());
+                    n2 = solve(toParse.substring(i));
+                    stack.push(Double.toString(multiply(n1, n2)));
+                }
             }
         }
 
